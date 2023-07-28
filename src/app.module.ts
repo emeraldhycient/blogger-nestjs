34aa635c4b2dc6d441340517920ac68294428968
emeraldhycient <igwezehycient86@gmail.com>
2023-07-28 11:53:45 +0100

    [update] added dto for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { MongoModule } from './mongo/mongo.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongoModule, PostModule, CommentModule, CategoryModule],
+  imports: [MongoModule, PostModule, CommentModule, CategoryModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
